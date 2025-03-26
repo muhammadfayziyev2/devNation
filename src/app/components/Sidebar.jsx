09ae0582d285cb40c44371935d2f6969472149d0
Muhammad Fayziyev <muhammadfayziyev@gmail.com>
2025-03-26 15:47:06 +0500

    new file:   src/app/[kurslar]/page.jsx 	new file:   src/app/[loyihalar]/page.jsx 	modified:   src/app/components/Header.jsx 	modified:   src/app/components/Sidebar.jsx 	modified:   src/app/globals.css 	modified:   src/app/layout.js

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,13 +1,28 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-        <div className='home'>
-
-        
-
-        </div>
+      <span className='span_dashboard'>Bo'limlar</span>
+      <div className='dashboard'>
+        <Link href="/loyihalar" className='home_dashboard'>
+          <svg className='icon_dashboard' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#ffffff" d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" /></svg>
+          <span className='a_dashboard' >Bosh sahifa</span>
+        </Link>
+        <Link href="/kurslar" className='home_dashboard'>
+          <svg className='icon_dashboard' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#ffffff" d="M0 80C0 53.5 21.5 32 48 32l96 0c26.5 0 48 21.5 48 48l0 16 192 0 0-16c0-26.5 21.5-48 48-48l96 0c26.5 0 48 21.5 48 48l0 96c0 26.5-21.5 48-48 48l-96 0c-26.5 0-48-21.5-48-48l0-16-192 0 0 16c0 1.7-.1 3.4-.3 5L272 288l96 0c26.5 0 48 21.5 48 48l0 96c0 26.5-21.5 48-48 48l-96 0c-26.5 0-48-21.5-48-48l0-96c0-1.7 .1-3.4 .3-5L144 224l-96 0c-26.5 0-48-21.5-48-48L0 80z" /></svg>
+          <span className='a_dashboard'>Loyihalar</span>
+        </Link>
+        <Link className='home_dashboard' href='/'>
+          <svg className='icon_dashboard' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#ffffff" d="M0 96C0 43 43 0 96 0l96 0 0 190.7c0 13.4 15.5 20.9 26 12.5L272 160l54 43.2c10.5 8.4 26 .9 26-12.5L352 0l32 0 32 0c17.7 0 32 14.3 32 32l0 320c0 17.7-14.3 32-32 32l0 64c17.7 0 32 14.3 32 32s-14.3 32-32 32l-32 0L96 512c-53 0-96-43-96-96L0 96zM64 416c0 17.7 14.3 32 32 32l256 0 0-64L96 384c-17.7 0-32 14.3-32 32z" /></svg>
+          <span className='a_dashboard'>Kurslar</span>
+        </Link>
+        <Link className='home_dashboard' href='/'>
+          <svg className='icon_dashboard' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ffffff" d="M399 384.2C376.9 345.8 335.4 320 288 320l-64 0c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z" /></svg>
+          <span className='a_dashboard'>Resume</span>
+        </Link>
+      </div>
     </div>
   )
 }
