9195d3f586a7810c67d3469ef9f5b483c40cdc23
ismoil4944 <oismoiljon08@gmail.com>
2025-03-26 13:58:27 +0500

    modified:   src/app/components/Main.jsx

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Main = () => {
   return (
     <div className='main'>
-        
+      <div className='main_news'>
+      DevNation - dasturlash kurslari
+       <p>DevNation - bu sizga dasturlashni o'rganishda yordam beradigan platforma. Pullik darslardagi ma'lumotlarni <p> bepul olishingiz mumkin. Barchasi bir joyda!</p></p> 
+        </div>
     </div>
   )
 }
