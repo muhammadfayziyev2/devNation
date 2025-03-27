643313d3c33b89f19f15ab7490b689bbd3fc557b
Muhammad Fayziyev <muhammadfayziyev@gmail.com>
2025-03-27 20:15:53 +0500

    deleted:    src/app/[kurslar]/page.jsx 	deleted:    src/app/[loyihalar]/page.jsx 	modified:   src/app/components/Header.jsx 	modified:   src/app/components/Sidebar.jsx 	modified:   src/app/globals.css 	modified:   src/app/page.js

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -11,10 +11,7 @@ export default function Header() {
               <span className="nikename">DevNation</span>
             </div>
             <div className="units">
-              <select name="" id="" className="select">
-                <option value="uz" className="option">uz</option>
-                <option value="ru" className="option">ru</option>
-              </select>
+              
               <button className="sidebarw"><div className="line"></div></button>
               <svg className="contactimg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ffffff" d="M96 0C60.7 0 32 28.7 32 64l0 384c0 35.3 28.7 64 64 64l288 0c35.3 0 64-28.7 64-64l0-384c0-35.3-28.7-64-64-64L96 0zM208 288l64 0c44.2 0 80 35.8 80 80c0 8.8-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16c0-44.2 35.8-80 80-80zm-32-96a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zM512 80c0-8.8-7.2-16-16-16s-16 7.2-16 16l0 64c0 8.8 7.2 16 16 16s16-7.2 16-16l0-64zM496 192c-8.8 0-16 7.2-16 16l0 64c0 8.8 7.2 16 16 16s16-7.2 16-16l0-64c0-8.8-7.2-16-16-16zm16 144c0-8.8-7.2-16-16-16s-16 7.2-16 16l0 64c0 8.8 7.2 16 16 16s16-7.2 16-16l0-64z" /></svg>
               <button className="enter"><span className="kirish">Kirish</span><svg className="kirishimg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ffffff" d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z" /></svg></button>
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 
 import Header from "./components/Header";
-import Main from "./components/Main";
 import Sidebar from "./components/Sidebar"
 
 export default function Home() {
